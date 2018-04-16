846efc2fd6e399e04edceff9b4272ac901e94b36
tazin-afrin <tazin.tumpa@gmail.com>
2018-04-16 13:40:28 -0400

    update read_draft1

--- kappa/read_draft1.py
+++ kappa/read_draft1.py
@@ -1,34 +1,52 @@
 from config import filesConfig
 import csv, re
 
-name = '-'
-cond = filesConfig.condB
-outputfile = 'draft1_2018argrewrite_2.txt'
-
 
 def save_draft(filename, essay):
     text_file = open(filename, "w")
     text_file.write(essay)
     text_file.close()
 
-with open(filesConfig.draft1_file) as csvfile:
-    reader = csv.DictReader(csvfile)
-    data = list(reader)
-    fields = reader.fieldnames
 
-    for row in data:
-        if row['RecipientFirstName'] == name:
-            print(row['RecipientFirstName'],row['RecipientLastName'],row['RecipientEmail'])
-            # this is the answer for step 2 to check if participants read the provided material
-            print(row['Q38'])
+def read_draft1(filename):
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = list(reader)
+        fields = reader.fieldnames
 
-            save_draft(cond + outputfile, row['Q8'])
+        for row in data:
+            if row['RecipientFirstName'] == name:
+                print(row['RecipientFirstName'],row['RecipientLastName'],row['RecipientEmail'])
+                # this is the answer for step 2 to check if participants read the provided material
+                print(row['Q38'])
 
-            print('\n\n')
-            # essay = row['Q8'].split("\n")
-            essay = re.split('\n{1,}',row['Q8'])
+                save_draft(cond + outputfile, row['Q8'])
 
-            for i in range(len(essay)):
-                id = 'FirstDraftParagraph'+str(i+1)+','
-                print(id+essay[i])
-                print('\n')
+                print('\n\n')
+                # essay = row['Q8'].split("\n")
+                essay = re.split('\n{1,}',row['Q8'])
+
+                for i in range(len(essay)):
+                    id = 'FirstDraftParagraph'+str(i+1)+','
+                    print(id+essay[i])
+                    print('\n')
+
+
+def read_draft2(filename):
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = list(reader)
+        fields = reader.fieldnames
+
+
+if __name__ == '__main__':
+
+    draft = 1
+    name = '-'
+    cond = filesConfig.condB
+    outputfile = 'draft1_2018argrewrite_2.txt'
+
+    if draft == 1:
+        read_draft1(filesConfig.draft1_file)
+    else:
+        read_draft2(filesConfig.draft2_file)
